ec8cc63638c5f332b19e24cd82c9f6976e71b7e0
Davii Zarshenas <daviizarshenas@gmail.com>
2025-09-11 02:53:33 +0000

    P3

--- HW1/P3.py
+++ HW1/P3.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(1.920, 2.020, 0.01)
+x = np.arange(1.920, 2.080, 0.001)
 factor_rep = (x-2)**9
-coeff_rep = x**9 - 18*x**8 + x**7 - x**6 + x**5 - x**4 + x**3 - x**2 + x - 512
+coeff_rep = x**9 - 18*x**8 + 144*x**7 - 672*x**6 + 2016*x**5 - 4032*x**4 + 5376*x**3 - 4608*x**2 + 2304*x - 512
 
 plt.figure()
 plt.title("Numerical Precision")
 plt.xlabel("x")
 plt.ylabel("p(x)")
 plt.plot(x, factor_rep, label='factored representation')
-
+plt.plot(x, coeff_rep, label='coefficient representation')
+plt.legend()
+plt.savefig('Numerical_Precision')
