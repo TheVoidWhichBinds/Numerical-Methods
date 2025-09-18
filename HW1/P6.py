afdb9bd913c452f4bef50f81f9de9bdcc9f7b184
Davii Zarshenas <daviizarshenas@gmail.com>
2025-09-18 14:51:07 -0400

    Update P6.py
    Created exponential function

--- HW1/P6.py
+++ HW1/P6.py
@@ -1,4 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = -5.5
+n = 30
+Taylor = np.zeros(30,1)
+
+def exponential(x)
+  sum = 0
+  for i in range n
+    sum = sum + x**i/i!
+    Taylor(i,1) = sum
+return Taylor
+
+print(exponential(-5.5))
+print(1/exponential(5.5))
+print((1/exponential(0.5))**11)
+
+
+
