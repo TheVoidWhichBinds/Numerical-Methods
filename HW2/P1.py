d42292933c02cf878f0044eaf86ef4fb2a71e1b8
Davii Zarshenas <daviizarshenas@gmail.com>
2025-10-01 05:02:09 +0000

    Plotted approx error

--- HW2/P1.py
+++ HW2/P1.py
@@ -2,43 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
+
+
 #### global variables ####
 a = 0
 b = 4
-N = np.linspace(1e-6, 1e-1, 100)
-########################
+N_min = int(1e1)
+N_max = int(1e4)
+
 
 
 #### function definitions ####
-def step_size():
+def f(x): #function
+    return np.exp(x)/(1 + 4*x**2)
 
-  return (b-a)/N
+def trap(h, N): #trapezoidal integral
+    total = 0.0
+    for i in range(1, N):
+        total += f(a + i*h)
+    return (h/2)*f(a) + h*total + (h/2)*f(b)
 
-
-
-def f(x): #function definition
-  return np.exp(x)/(1 + 4*x**2)
-
-def trap(): #trapezoidal integral 
-  N 
-  sum = 0
-  for i in range(1,N): 
-    sum += f(a + i*h)
-  return (h/2)*f(a) + h*sum + (h/2)*f(b)
-
-integ_exact = integrate.quad(lambda x: f(x), a, b) #limit of integral
-#####################################################################
-
-
-#### plotting arrays ####
-u_h = np.empty()
+u_exact = integrate.quad(lambda x: f(x), a, b)[0] #exact value
 
 
 
+#### choose N values (logarithmic spacing) ####
+N_values = np.logspace(np.log10(N_min), np.log10(N_max), num=20, dtype=int)
 
+u_h = np.empty(len(N_values)) #array of approximated solutions at diff step sizes
+h_vals = (b - a)/N_values #array of step sizes
+
+for i, N in enumerate(N_values):
+    h = (b - a)/N #single step-size for each N_value
+    u_h[i] = trap(h, N) #entry into approx integral array for each step-size and N_value
+
+
+
+#### plotting ####
 plt.figure()
 plt.title("Trapezoidal Rule Convergence")
-plt.xlabel(" ")
-plt.ylabel(" ")
+plt.xlabel("Step Size")
+plt.ylabel(r"Approximation Error  $|u_h - u|$")
+plt.loglog(h_vals, np.abs(u_h - u_exact), marker='o')
+plt.grid(True, which="both", ls=":")
+plt.savefig("Convergence_Order.png")
 
-  
