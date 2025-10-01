42c658bf5727aa89ab8ba22011cf3bba28c22ce6
Davii Zarshenas <daviizarshenas@gmail.com>
2025-10-01 00:06:30 -0400

    Update P1.py

--- HW2/P1.py
+++ HW2/P1.py
@@ -1,3 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 
+#### global variables ####
+N_max = 1000 #number of steps
+a = 0
+b = 4
+h = (b-a)/N
+########################
+
+
+#### function definitions ####
+def f(x): #function definition
+  return np.exp(x)/(1 + 4*x**2)
+
+def trap(): #trapezoidal integral 
+  N 
+  sum = 0
+  for i in range(1,N): 
+    sum += f(a + i*h)
+  return (h/2)*f(a) + h*sum + (h/2)*f(b)
+
+integ_exact = integrate.quad(lambda x: f(x), a, b) #limit of integral
+#####################################################################
+
+
+#### plotting arrays ####
+u_h = np.empty()
+
+
+
+
+plt.figure()
+plt.title("Trapezoidal Rule Convergence")
+plt.xlabel(" ")
+plt.ylabel(" ")
+plt.plot(
+  
