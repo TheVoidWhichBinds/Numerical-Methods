e6792a17e450f797ead2052dfc1da7897e7aa68c
Davii Zarshenas <dz2596@nyu.edu>
2025-10-16 14:36:08 -0400

    lakdj

--- HW3/P3.py
+++ HW3/P3.py
@@ -1,2 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+#----- Generating random data -----#
+t = 2 * np.random.rand(20, 1)
+b = 1 + 0.8*t - 0.5*t**2 + 0.3*np.exp(t) + 0.2*np.random.randn(20, 1)
+
+
+
+
+
+
+
+
+
+#----- Plotting the random data -----#
+plt.plot(t, b, 'ro')
+plt.xlabel('t')
+plt.ylabel('b')
+plt.savefig('Data.png')
