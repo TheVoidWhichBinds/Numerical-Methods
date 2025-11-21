3e7e2e13700e884ef430711a3e9c011c9a180503
Davii Zarshenas <dz2596@nyu.edu>
2025-11-21 02:23:23 -0500

    Newton's Method P2.py

--- HW6/P2.py
+++ HW6/P2.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+########################################## SUBSECTION A ########################################## 
 
 k_opt = -0.404036086 #weight parameter
 k_range = np.arange(-0.7,0,0.02)
 
 #------- Functions -------#
 #Exact function:
-def f(x):
+def f1(x):
     return x*np.exp(x) - 1
 
 #Iterative solver:
@@ -17,7 +18,7 @@ def fixed_point(k):
     x = x_0 #starting value
 
     for i in range(max_iter):
-        x_new = x + k*f(x)
+        x_new = x + k*f1(x)
         if np.abs(x_new - x) < 1E-8:
             break
         x = x_new
@@ -34,9 +35,43 @@ iters = []
 for k in k_range:
     iters.append(fixed_point(k))
 plt.scatter(k_range, iters, color='magenta')
-plt.show()
+#plt.show()
 plt.savefig('fixed_pt.png')
 
 print(f'k_opt iterations: {fixed_point(k_opt)}')
     
 
+
+
+
+
+########################################## SUBSECTION C ########################################## 
+
+
+
+def f2(x):
+    return 1/x + 2*np.log(x) - 2
+
+def f2_prime(x):
+    return -1/x**2 + 2/x 
+
+def newton_method(init_guess):
+    x = init_guess #initial guess
+    num_iter = 10
+    for i in range(num_iter):
+        x_new = x - f2(x)/f2_prime(x)
+        x = x_new
+        print(x_new)
+
+    return x_new
+
+
+x = np.linspace(0.1,3,100)
+
+
+plt.figure()
+plt.title('')
+plt.plot(x, f2(x), label='f(x)')
+plt.scatter([newton_method(0.1), newton_method(2)], [0,0], label='roots')
+plt.grid(True)
+#plt.show()
