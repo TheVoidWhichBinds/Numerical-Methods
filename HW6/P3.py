225b089353869f3d04268c1c5646e7b1b12266fb
Davii Zarshenas <dz2596@nyu.edu>
2025-11-25 17:01:03 -0500

    finalized P3

--- HW6/P3.py
+++ HW6/P3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 T = np.array([
   -4.100000000000000,
@@ -22,29 +23,66 @@ F = np.array([
     0.091759658119148,
     0.065019274935861,
     0.005349325963815])
+NUM_GAUSS = 2
 
-def phi(num_gaussians,x):
-    phi = 0
-    for g in range(num_gaussians):
-       #x[3g]=omega, x[3g+1]=sigma, x[3g+2]=tau
+
+def residual(x):
+    phi = np.zeros((len(T))) #initializing Gaussian superposition
+    for g in range(NUM_GAUSS):
+       #x[3g]=omega, x[3g+1]=sigma, x[3g+2]=tau:
        phi +=  x[3*g] * np.exp(-1/(2 * x[3*g + 1]**2) * (T - x[3*g + 2])**2)
-
     
+    res = phi - F #calculating the residual
+    return res
 
 
-def residual(num_gaussians,x):
-    return phi(num_gaussians,x) - F
-
-
-def res_jacobian(num_gaussians,x):
-    J = np.empty((len(T),3*num_gaussians))
-    for i in range(len(T)):
-        for g in range(0,num_gaussians):
+def res_jacobian(x):
+    J = np.empty((len(T),3*NUM_GAUSS))
+    for i in range(len(T)): #rows = data points
+        for g in range(0,NUM_GAUSS):  #columns = wrt 6 Gaussian parameters
+            #x[3g]=omega, x[3g+1]=sigma, x[3g+2]=tau:
             J[i,3*g] = np.exp(-1/(2 * x[3*g + 1]**2) * (T[i] - x[3*g + 2])**2)
-            J[i,3*g+1] = 
-            J[i,3*g+2] = 
-    
+            J[i,3*g+1] = (x[3*g] * np.exp(-1/(2 * x[3*g + 1]**2) * (T[i] - x[3*g + 2])**2) 
+                         * (T[i] - x[3*g + 2])**2 / x[3*g + 1]**3)
+            J[i,3*g+2] = (x[3*g] * np.exp(-1/(2 * x[3*g + 1]**2) * (T[i] - x[3*g + 2])**2) 
+                         * (T[i] - x[3*g + 2]) / x[3*g + 1]**2)
+    return J
     
 
+def gauss_newton(x_0):
+    x = x_0
+    x_k = [x.copy()]
+    max_iter = 100
+    Delta_x = 1
+    for k in range(max_iter):
+        Q,R = np.linalg.qr(res_jacobian(x))
+        Delta_x = np.linalg.solve(R,-Q.T@residual(x))
+        x += Delta_x
+        x_k.append(x.copy())
+        if np.linalg.norm(Delta_x) < 1E-16:
+            break 
+    return np.array(x_k)
 
-def gauss_newton():
+
+def error_array():
+    iterations = gauss_newton(np.array([1,1,-3,1,1,1.5])) #calling gauss-newton solver
+    x_star = iterations[-1] #solution 
+    errors = np.linalg.norm((iterations - x_star), axis=1) #error = 2-norm(iteration sol - final sol)
+    return errors
+
+
+def convergence_order():
+    iterations = gauss_newton(np.array([1,1,-3,1,1,1.5])) #solution at each iteration
+    x_star = iterations[-1] #iteratively solved solution
+    errors = np.linalg.norm(iterations - x_star, axis=1) #errors at each iteration
+    e = errors[errors > 1e-15] #removing ~ zero errors so no log(0) issue arises
+    #Convergence order:
+    p_vals = []
+    for k in range(1, len(e)-1):
+        p_vals.append(np.log(e[k+1]/e[k]) / np.log(e[k] / e[k-1]))
+    p_vals = np.array(p_vals)
+    return p_vals[-3:].mean() #average of last 3 estimates (G-N errors initially erratic)
+    
+print(np.round(convergence_order(),2))
+
+
